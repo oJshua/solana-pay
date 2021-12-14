cd2e9d012f52e4e0ec5d3e5bf7def740c3f68b5a
Josh Hundley <josh.hundley@gmail.com>
2021-12-14 10:51:27 -0800

    feat: oauth redirect

--- payment-api/src/integrations/shopify/onboarding/onboarding.controller.ts
+++ payment-api/src/integrations/shopify/onboarding/onboarding.controller.ts
@@ -1,22 +1,30 @@
-import { Body, Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, Query, Res } from '@nestjs/common';
 import { InstallRequestDto } from 'src/integrations/shopify/dto/install-request.dto';
-
-const CLIENT_ID = '';
 
 @Controller('integrations/shopify/onboarding')
 export class OnboardingController {
 
   @Get('install')
-  async function(@Body() body: InstallRequestDto, @Res() res) {
-
-
-
+  async install(@Query() { shop }: InstallRequestDto, @Res() res) {
     return res.redirect(this.getShopifyUrl(
-      body.shop
+      shop
     ));
   }
 
+  @Get('redirect')
+  async redirect(@Query() test: any, @Res() res) {
+    console.log(test);
+    return true;
+  }
+
   getShopifyUrl(shop: string) {
-    return `https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${CLIENT_ID}`;
+    let url = `https://${shop}.myshopify.com/admin/oauth/authorize?`;
+    url += `client_id=${process.env.SHOPIFY_API_KEY}`
+    url += `&scope=write_payment_gateways,write_payment_sessions`;
+    url += `&redirect_uri=${process.env.BACKEND_URL}/v1/integrations/shopify/onboarding/redirect`;
+    url += `&nonce=${Math.floor(Math.random() * 10000000)}`;
+    url += `&grant_options[]=value`;
+
+    return url;
   }
 }
